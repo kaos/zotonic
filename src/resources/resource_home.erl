9cc84555253cc81e9b04fd917c065c57e009140c
tim <tim@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-02-26 00:49:06 +0000

    After adding this resource, I now added some basic events in it. Just to test.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@16 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- src/resources/resource_home.erl
+++ src/resources/resource_home.erl
@@ -1,11 +1,22 @@
 %% @author Tim Benniks <tim@timbenniks.com>
 %% @copyright 2009 Tim Benniks.
-%% @doc Example webmachine_resource.
+%% @doc Home Page webmachine_resource.
 
- -module(resource_home).
- -author("Tim Benniks <tim@timbenniks.com>").
- -include_lib("resource_html.hrl").
+-module(resource_home).
+-export([event/2]).
+-author("Tim Benniks <tim@timbenniks.com>").
+-include_lib("resource_html.hrl").
  
- html(_ReqProps, Context) ->
+html(_ReqProps, Context) ->
     Html = zp_template:render("home.tpl", Context),
     zp_context:output(Html, Context).    
+
+event({postback, show_growl_search, _TriggerId, _TargetId}, Context) ->
+    zp_render:wire({growl, [{text,"You clicked on search! Sweet..."}]}, Context);
+
+event({postback, show_growl_newsletter, _TriggerId, _TargetId}, Context) ->
+    zp_render:wire({growl, [{text,"YÅou have clicked on newsletter! Sweeter!"}]}, Context);
+
+event(Event, Context) ->
+    Error = io_lib:format("~p: unknown event ~p", [?MODULE,Event]),
+    zp_render:wire({growl, [{text,Error},{stay,1}]}, Context).
