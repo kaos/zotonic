49a1f1c403a60ede6fb07accad00833b603610fa
tim <tim@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-02-28 10:44:52 +0000

    Added two other resources.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@31 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

---
+++ priv/resources/resource_page.erl
@@ -0,0 +1,13 @@
+%% @author Tim Benniks <tim@timbenniks.com>
+%% @copyright 2009 Tim Benniks.
+%% @doc Home Page webmachine_resource.
+
+-module(resource_page).
+-author("Tim Benniks <tim@timbenniks.com>").
+-include_lib("resource_html.hrl").
+
+html(_ReqProps, Context) ->
+    Html = zp_template:render("page.tpl", Context),
+    zp_context:output(Html, Context).
+   
+
