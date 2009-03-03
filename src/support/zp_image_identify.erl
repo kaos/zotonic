3924d452e8be70f8c3eb6c24920e181267deb87b
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-03-03 21:47:34 +0000

    Image manipulation, a Work In Progress.
    refs #14
    
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@71 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

---
+++ src/support/zp_image_identify.erl
@@ -0,0 +1,63 @@
+%% @author Marc Worrell <marc@worrell.nl>
+%% @copyright 2009 Marc Worrell
+%% @date 2009-03-02
+%%
+%% @doc Identify images, fetch metadata about an image
+
+-module(zp_image_identify).
+-author("Marc Worrell <marc@worrell.nl").
+
+%% interface functions
+-export([
+    identify_cached/1,
+    identify/1
+]).
+
+-include_lib("zophrenic.hrl").
+
+
+%% @spec identify(ImageFile) -> {ok, Meta} | {error, Error}
+%% @doc Caching version of identify/1. Fetches information about an image, returns width, height, type, etc.
+identify_cached(ImageFile) ->
+    case zp_depcache:get({identify, ImageFile}) of
+        {ok, Meta} -> Meta;
+        undefined ->
+            Result = identify(ImageFile),
+            zp_depcache:set({identify, ImageFile}, Result, ?YEAR, [identify, ImageFile]),
+            Result
+    end.
+    
+    
+%% @spec identify(ImageFile) -> PropList
+%% @doc Fetch information about an image, returns width, height, type, etc.
+identify(ImageFile) ->
+    CleanedImageFile = zp_utils:os_escape(ImageFile),
+    Result    = os:cmd("identify -quiet \"" ++ CleanedImageFile ++ "\""),
+    [Line1|_] = string:tokens(Result, "\r\n"),
+    case string:tokens(Line1, " ") of
+        % ["test/a.jpg","JPEG","3440x2285","3440x2285+0+0","8-bit","DirectClass","2.899mb"]
+        [_Path, Type, Dim, _Dim2, _Depth, _Class, _Size] ->
+            [Width,Height] = string:tokens(Dim, "x"),
+            {ok, [
+                {width, list_to_integer(Width)},
+                {height, list_to_integer(Height)},
+                {mime, mime(Type)},
+                {orientation, 1}
+            ]};
+        _ ->
+            {error, "unknown result from 'identify': '"++Line1++"'"}
+    end.
+
+
+%% @spec mime(String) -> MimeType
+%% @doc Map the type returned by ImageMagick to a mime type
+%% @todo Add more imagemagick types, check the mime types
+mime("PDF") -> "application/pdf";
+mime("PS") -> "application/postscript";
+mime("PS2") -> "application/postscript";
+mime("PS3") -> "application/postscript";
+mime("PNG8") -> "image/png";
+mime("PNG24") -> "image/png";
+mime("PNG32") -> "image/png";
+mime(Type) -> "image/" ++ string:to_lower(Type).
+
---
+++ src/support/zp_image_tag.erl
@@ -0,0 +1,51 @@
+%% @author Marc Worrell <marc@worrell.nl>
+%% @copyright 2009 Marc Worrell
+%% @date 2009-03-03
+%%
+%% @doc Generate image urls and tags, based on the filename, size and optional filters.
+%% Does not generate the image itself, this is done when fetching the image.
+
+-module(zp_image_tag).
+-author("Marc Worrell <marc@worrell.nl").
+
+%% interface functions
+-export([
+    tag/2,
+    url/2
+]).
+
+
+%% @spec tag(Filename, Options) -> {tag, TagString} | {error, Reason}
+%% @doc Generate a HTML image tag for the image with the filename and options
+tag(Filename, Options) ->
+    case url1(Filename, Options) of
+        {url, Url, ExtraOptions} -> 
+            {tag, Url};
+        {error, Reason} -> 
+            {error, Reason}
+    end.
+    
+
+%% @spec url(Filename, Options) -> {url, Url} | {error, Reason}
+%% @doc Generate the url for the image with the filename and options
+url(Filename, Options) ->
+    {error, not_implemented}.
+
+
+%% @spec url1(Filename, Options) -> {url, Url, TagOptions} | {error, Reason}
+%% @doc Creates an url for the given filename and filters.
+%% Typical urls are like: image/2007/03/31/wedding-300x300-crop-north_east-grey-a3ab6605e5c8ce801ac77eb76289ac12.jpg
+url1(Filename, Options) ->
+    {TagOpts, ImageOpts} = lists:partition(fun is_tagopt/1, Options).
+    case zp_image_convert:size(Filename, ImageOpts) of
+        {size, Width, Height, Mime} ->
+             
+        {error, Reason} -> {error, Reason}
+    end.
+        
+is_tagopt({alt,   _}) -> true;
+is_tagopt({title, _}) -> true;
+is_tagopt({class, _}) -> true;
+is_tagopt({style, _}) -> true;
+is_tagopt(_) -> false.
+    
