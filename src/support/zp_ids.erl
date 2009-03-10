4c31b822e6535053be16d844a562b9ba586a7db0
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-03-10 20:20:42 +0000

    Added identifier(), which makes a random string suitable as a case insensitive identifier.
    Added more randomness to the id() output, lower case, upper case and digits.
    
    optid() calls now identifier().
    
    Added sign_key_simple(), which is used for signing things without nonces (like image uris).
    
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@94 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- src/support/zp_ids.erl
+++ src/support/zp_ids.erl
@@ -22,14 +22,17 @@
     unique/0, 
     id/0,
     id/1,
+    identifier/0,
+    identifier/1,
     optid/1,
     sign_key/0,
+    sign_key_simple/0,
     number/0,
     number/1,
     start_link/0
 ]).
 
--record(state, {sign_key}).
+-record(state, {sign_key, sign_key_simple}).
 
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
@@ -44,14 +47,28 @@ id() ->
 id(Len) -> 
     gen_server:call(?MODULE, {id, Len}).
 
-optid(undefined) -> id(?OPTID_LENGTH);
-optid(false) -> id(?OPTID_LENGTH);
+%% @spec identifier() -> binary()
+%% @doc Get a random indentifier of a certain length, case insensitive
+identifier() -> 
+    gen_server:call(?MODULE, {identifier, ?OPTID_LENGTH}).
+
+identifier(Len) -> 
+    gen_server:call(?MODULE, {identifier, Len}).
+
+optid(undefined) -> identifier(?OPTID_LENGTH);
+optid(false) -> identifier(?OPTID_LENGTH);
 optid(Id) -> Id.
 
 %% @spec sign_key() -> binary()
 %% @doc Get the key for signing requests stored in the user agent.
 sign_key() -> 
     gen_server:call(?MODULE, sign_key).
+
+
+%% @spec sign_key_simple() -> binary()
+%% @doc Get the key for less secure signing of data (without nonce).
+sign_key_simple() -> 
+    gen_server:call(?MODULE, sign_key_simple).
     
 
 %% @doc Return a big random integer, but smaller than maxint32
@@ -80,6 +97,10 @@ handle_call({number, Max}, _From, State) ->
     Number = random:uniform(Max), 
     {reply, Number, State};
 
+handle_call({identifier, Len}, _From, State) ->
+    Id = generate_identifier(Len),
+    {reply, Id, State};
+
 handle_call({id, Len}, _From, State) ->
     Id = generate_id(Len),
     {reply, Id, State};
@@ -87,9 +108,19 @@ handle_call({id, Len}, _From, State) ->
 handle_call(sign_key, _From, State) ->
     case State#state.sign_key of
         undefined ->
-            SKey = case os:getenv("ZOPHRENIC_SIGN_KEY") of false -> generate_id(100); K -> K end,
+            SKey = case os:getenv("ZOPHRENIC_SIGN_KEY") of false -> generate_id(50); K -> K end,
             Key  = list_to_binary(SKey),
             {reply, Key, State#state{sign_key=Key}};
+        Key -> 
+            {reply, Key, State}
+    end;
+
+handle_call(sign_key_simple, _From, State) ->
+    case State#state.sign_key_simple of
+        undefined ->
+            SKey = case os:getenv("ZOPHRENIC_SIGN_KEY_SIMPLE") of false -> generate_id(10); K -> K end,
+            Key  = list_to_binary(SKey),
+            {reply, Key, State#state{sign_key_simple=Key}};
         Key -> 
             {reply, Key, State}
     end;
@@ -120,12 +151,28 @@ unique1([_|T], Acc) ->
 
 
 %% @spec generate_id(int()) -> string()
-%% @doc Generate a random session key, stored in a cookie in the user agent
+%% @doc Generate a random key
 generate_id(Len) ->
     generate_id(Len, []).
 
 generate_id(0, Key) ->
     Key;
 generate_id(Len, Key) ->
-    generate_id(Len-1, [random:uniform(26)+96|Key]).
+    Char = case random:uniform(62) of
+                C when C =< 26 -> C - 1  + $a;
+                C when C =< 52 -> C - 27 + $A;
+                C -> C - 53 + $0
+            end,
+    generate_id(Len-1, [Char|Key]).
+
+
+%% @spec generate_identifier(int()) -> string()
+%% @doc Generate a random identifier, case insensitive, only letters
+generate_identifier(Len) ->
+    generate_identifier(Len, []).
+
+generate_identifier(0, Key) ->
+    Key;
+generate_identifier(Len, Key) ->
+    generate_identifier(Len-1, [random:uniform(26)-1+$a|Key]).
 
