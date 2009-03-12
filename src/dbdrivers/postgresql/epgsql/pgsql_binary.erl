4592061d0cf3c8b54fa4a334df8ec1e7697ce62e
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-03-12 13:06:00 +0000

    Removed .app files Added support for storing terms in bytea fields.  This is done with a magic marker prefix to distinguish serialized terms from 'normal' binaries.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@106 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- src/dbdrivers/postgresql/epgsql/pgsql_binary.erl
+++ src/dbdrivers/postgresql/epgsql/pgsql_binary.erl
@@ -1,10 +1,13 @@
 %%% Copyright (C) 2008 - Will Glozer.  All rights reserved.
+%%% 20090311 Marc Worrell - Added support for encoding terms and lists of terms in bytea values
 
 -module(pgsql_binary).
 
 -export([encode/2, decode/2, supports/1]).
 
 -define(int32, 1/big-signed-unit:32).
+-define(TERM_MAGIC_NUMBER, 16#01326A3A:1/big-unsigned-unit:32).
+
 
 encode(_Any, null)  -> <<-1:?int32>>;
 encode(bool, true)  -> <<1:?int32, 1:1/big-signed-unit:8>>;
@@ -21,7 +24,13 @@ encode(Type, B) when Type == timestamptz; Type == interval -> pgsql_datetime:enc
 encode(bytea, B) when is_binary(B)   -> <<(byte_size(B)):?int32, B/binary>>;
 encode(text, B) when is_binary(B)    -> <<(byte_size(B)):?int32, B/binary>>;
 encode(varchar, B) when is_binary(B) -> <<(byte_size(B)):?int32, B/binary>>;
-encode(Type, L) when is_list(L)      -> encode(Type, list_to_binary(L));
+encode(bytea, T) when is_tuple(T)   -> 
+    B = term_to_binary(T),
+    encode(bytea, <<?TERM_MAGIC_NUMBER, B/binary>>);
+encode(bytea, [T|_Rest]=L) when is_tuple(T)   -> 
+    B = term_to_binary(L),
+    encode(bytea, <<?TERM_MAGIC_NUMBER, B/binary>>);
+encode(Type, L) when is_list(L)      -> encode(Type, iolist_to_binary(L));
 encode(_Type, _Value)                -> {error, unsupported}.
 
 decode(bool, <<1:1/big-signed-unit:8>>)     -> true;
@@ -36,6 +45,7 @@ decode(record, <<_:?int32, Rest/binary>>)   -> list_to_tuple(decode_record(Rest,
 decode(Type, B) when Type == time; Type == timetz          -> pgsql_datetime:decode(Type, B);
 decode(Type, B) when Type == date; Type == timestamp       -> pgsql_datetime:decode(Type, B);
 decode(Type, B) when Type == timestamptz; Type == interval -> pgsql_datetime:decode(Type, B);
+decode(bytea, <<?TERM_MAGIC_NUMBER, B/binary>>) -> binary_to_term(B);
 decode(_Other, Bin) -> Bin.
 
 decode_record(<<>>, Acc) ->
