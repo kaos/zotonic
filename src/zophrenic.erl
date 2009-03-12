5e8760805e2910a683f6548f8d247e499e128b39
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-03-12 10:35:22 +0000

    Added stop([Node]) for stopping a zophrenic server running at a node.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@103 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- src/zophrenic.erl
+++ src/zophrenic.erl
@@ -1,7 +1,7 @@
 %% @author Marc Worrell <marc@worrell.nl>
 %% @copyright 2009 Marc Worrell
 
-%% @doc Basic functions for Zophrenic
+%% @doc Start/stop functions for Zophrenic
 
 -module(zophrenic).
 -author('Marc Worrell <marc@worrell.nl>').
@@ -15,10 +15,14 @@ ensure_started(App) ->
 	{error, {already_started, App}} ->
 	    ok
     end.
-	
+
 %% @spec start() -> ok
 %% @doc Start the zophrenic server.
-start() ->
+start() -> start([]).
+	
+%% @spec start(_Args) -> ok
+%% @doc Start the zophrenic server.
+start(_Args) ->
     zophrenic_deps:ensure(),
     ensure_started(crypto),
     ensure_started(webmachine),
@@ -32,3 +36,13 @@ stop() ->
     application:stop(crypto),
     Res.
 
+
+%% @spec stop([Node]) -> void()
+%% @doc Stop a zophrenic server on a specific node
+stop([Node]) ->
+    io:format("Stop:~p~n",[Node]),
+    case net_adm:ping(Node) of
+    	pong -> rpc:cast(Node, init, stop, []);
+    	pang -> io:format("There is no node with this name~n")
+    end,
+    init:stop().
