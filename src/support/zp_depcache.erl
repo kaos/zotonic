99449099cf4ebc8f1ed9e73e4aa0f0ca3c1074ec
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-04-07 19:14:55 +0000

    Now uses the process dictionary to store the data. This overcomes the copy delay from/to ets, which especially speeds up when we have big data entries.
    You can now also request a subkey from a structured entry, this prevents copying the whole entry over to the calling process when you only want a single field.
    
    Now uses erts_debug:flat_size() to keep track of the size of the stored terms.
    
    
    
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@147 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- src/support/zp_depcache.erl
+++ src/support/zp_depcache.erl
@@ -12,23 +12,23 @@
 -export([start_link/0]).
 
 %% depcache exports
--export([set/2, set/3, set/4, get/1, flush/1, flush/0, tick/0]).
+-export([set/2, set/3, set/4, get/1, get/2, flush/1, flush/0, tick/0, size/0]).
 
 %% internal export
--export([cleanup/4]).
+-export([cleanup/1, cleanup/5]).
 
--record(state, {now, serial}).
+-include_lib("zophrenic.hrl").
+
+-record(state, {now, serial, size=0}).
 -record(meta,  {key, expire, serial, depend}).
--record(data,  {key, data}).
 -record(depend,{key, serial}).
 
 start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 
--define(DATA_TABLE, zp_depcache_data).
 -define(META_TABLE, zp_depcache_meta).
 -define(DEPS_TABLE, zp_depcache_deps).
--define(MEMORY_MAX, 10*1024*1024).
+-define(MEMORY_MAX, 20*1024*1024).
 
 % Number of slots visited for each iteration
 -define(CLEANUP_BATCH, 100).
@@ -37,25 +37,34 @@ start_link() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 %% @spec set(Key, Data, MaxAge) -> void()
 %% @doc Add the key to the depcache, hold it for 3600 seconds and no dependencies
 set(Key, Data) ->
-    gen_server:cast(?MODULE, {set, Key, Data, 3600, []}).
+    Size = erts_debug:flat_size(Data),
+    gen_server:call(?MODULE, {set, Key, Data, Size, 3600, []}).
 
 
 %% @spec set(Key, Data, MaxAge) -> void()
 %% @doc Add the key to the depcache, hold it for MaxAge seconds and no dependencies
 set(Key, Data, MaxAge) ->
-    gen_server:cast(?MODULE, {set, Key, Data, MaxAge, []}).
+    Size = erts_debug:flat_size(Data),
+    gen_server:call(?MODULE, {set, Key, Data, Size, MaxAge, []}).
 
 
 %% @spec set(Key, Data, MaxAge, Depend) -> void()
 %% @doc Add the key to the depcache, hold it for MaxAge seconds and check the dependencies
 set(Key, Data, MaxAge, Depend) ->
-    gen_server:cast(?MODULE, {set, Key, Data, MaxAge, Depend}).
+    Size = erts_debug:flat_size(Data),
+    gen_server:call(?MODULE, {set, Key, Data, Size, MaxAge, Depend}).
 
 
 %% @spec get(Key) -> {ok, Data} | undefined
 %% @doc Fetch the key from the cache, return the data or an undefined if not found (or not valid)
 get(Key) ->
     gen_server:call(?MODULE, {get, Key}).
+
+
+%% @spec get(Key, SubKey) -> {ok, Data} | undefined
+%% @doc Fetch the key from the cache, return the data or an undefined if not found (or not valid)
+get(Key, SubKey) ->
+    gen_server:call(?MODULE, {get, Key, SubKey}).
 
 
 %% @spec flush(Key) -> void()
@@ -74,18 +83,23 @@ tick() ->
     gen_server:cast(?MODULE, tick).
 
 
+%% @spec size() -> int()
+%% @doc Return the total size of all stored terms
+size() ->
+    gen_server:call(?MODULE, size).
+    
+
 %% gen_server callbacks
 
 %% @spec init([]) -> {ok, session_srv())}
 %% @doc Initialize the session server with an empty session table.  We make the session manager a system process
 %%      so that crashes in sessions are isolated from each other.
 init(_Args) ->
-    ets:new(?DATA_TABLE, [set, named_table, {keypos, 2}, protected]),
     ets:new(?META_TABLE, [set, named_table, {keypos, 2}, protected]),
     ets:new(?DEPS_TABLE, [set, named_table, {keypos, 2}, protected]),
     State = #state{now=zp_utils:now(), serial=0},
     timer:apply_interval(1000, ?MODULE, tick, []),
-    spawn_link(fun cleanup/0),
+    spawn_link(?MODULE, cleanup, [self()]),
     {ok, State}.
 
 %% @doc Fetch a key from the cache, returns undefined when not found.
@@ -99,25 +113,43 @@ handle_call({get, Key}, _From, State) ->
             case Expire >= State#state.now of
                 false -> 
                     ets:delete(?META_TABLE, Key),
-                    ets:delete(?DATA_TABLE, Key),
-                    {reply, undefined, State};
+                    State1 = erase_key(Key, State),
+                    {reply, undefined, State1};
                 true ->
                     %% Check dependencies
                     case check_depend(Serial, Depend) of
                         true ->
-                            [Data] = ets:lookup(?DATA_TABLE, Key),
-                            {reply, {ok, Data#data.data}, State};
+                            case erlang:get(Key) of
+                                {ok, _, Data} -> {reply, {ok, Data}, State};
+                                _ -> {reply, undefined, State}
+                            end;
                         false ->
                             ets:delete(?META_TABLE, Key),
-                            ets:delete(?DATA_TABLE, Key),
-                            {reply, undefined, State}
+                            State1 = erase_key(Key, State),
+                            {reply, undefined, State1}
                     end
             end
-    end.
+    end;
+
+%% @doc Fetch a subkey from a key from the cache, returns undefined when not found.
+%% This is useful when the cached data is very large and the fetched data is small in comparison.
+handle_call({get, Key, SubKey}, From, State) ->
+    {reply, Data, State1} = handle_call({get, Key}, From, State),
+    case Data of
+        {ok, Value} ->
+            {reply, {ok, find_value(SubKey, Value)}, State1};
+        _ ->
+            {reply, Data, State1}
+    end;
+
+
+%% @doc Return the size of all stored terms
+handle_call(size, _From, State) ->
+    {reply, State#state.size, State};
 
 
 %% Add an entry to the cache table
-handle_cast({set, Key, Data, MaxAge, Depend}, State) ->
+handle_call({set, Key, Data, Size, MaxAge, Depend}, _From, State) ->
     %% On every update we inc the serial, flushing depending items
     State1 = State#state{serial=State#state.serial+1},
     
@@ -130,26 +162,25 @@ handle_cast({set, Key, Data, MaxAge, Depend}, State) ->
     %% Flush the key itself in the dependency table - this will invalidate depending items
     case is_simple_key(Key) of
         true  -> ok;
-        false -> 
-            ets:insert(?DEPS_TABLE, #depend{key=Key, serial=State#state.serial})
+        false -> ets:insert(?DEPS_TABLE, #depend{key=Key, serial=State#state.serial})
     end,
 
     %% Insert the record into the cache table
-    ets:insert(?DATA_TABLE, #data{key=Key, data=Data}),
+    erlang:put(Key, {ok, Size, Data}),
     ets:insert(?META_TABLE, #meta{key=Key, expire=State1#state.now+MaxAge, serial=State1#state.serial, depend=Depend}),
-    {noreply, State1};
+    {reply, ok, State1#state{size=State1#state.size + Size}}.
 
 handle_cast({flush, Key}, State) ->
     ets:delete(?DEPS_TABLE, Key),
     ets:delete(?META_TABLE, Key),
-    ets:delete(?DATA_TABLE, Key),
-    {noreply, State};
+    State1 = erase_key(Key, State),
+    {noreply, State1};
 
 handle_cast(flush, State) ->
     ets:delete_all_objects(?META_TABLE),
     ets:delete_all_objects(?DEPS_TABLE),
-    ets:delete_all_objects(?DATA_TABLE),
-    {noreply, State};
+    erlang:erase(),
+    {noreply, State#state{size=0}};
 
 handle_cast(tick, State) ->
     {noreply, State#state{now=zp_utils:now()}};
@@ -174,6 +205,17 @@ is_simple_key(_Key) ->
     true.
 
 
+%% @doc Erase the key from the process dictionary, substract the size from the accumulator.
+erase_key(Key, State) ->
+    Data = erlang:erase(Key),
+    case Data of
+        {ok, Size, _} ->
+            State#state{size = State#state.size - Size};
+        undefined ->
+            State
+    end.
+
+
 %% @doc Check if all dependencies are still valid, that is they have a serial before or equal to the serial of the entry
 check_depend(_Serial, []) ->
     true;
@@ -189,63 +231,134 @@ check_depend(Serial, Depend) ->
     lists:foldl(CheckDepend, true, Depend).
 
 
+
+% Index of list with an integer like "a[2]"
+find_value(Key, L) when is_integer(Key) andalso is_list(L) ->
+    try
+        lists:nth(Key, L)
+    catch
+        _:_ -> undefined
+    end;
+find_value(Key, {GBSize, GBData}) when is_integer(GBSize) ->
+    case gb_trees:lookup(Key, {GBSize, GBData}) of
+        {value, Val} ->
+            Val;
+        _ ->
+            undefined
+    end;
+
+%% Regular proplist lookup
+find_value(Key, L) when is_list(L) ->
+    proplists:get_value(Key, L);
+
+%% Resource list handling, special lookups when skipping the index
+find_value(Key, {rsc_list, L}) when is_integer(Key) ->
+    try
+        lists:nth(Key, L)
+    catch
+        _:_ -> undefined
+    end;
+find_value(Key, {rsc_list, [H|_T]}) ->
+	find_value(Key, H);
+find_value(_Key, {rsc_list, []}) ->
+	undefined;
+
+% Index of tuple with an integer like "a[2]"
+find_value(Key, T) when is_integer(Key) andalso is_tuple(T) ->
+    try
+        element(Key, T)
+    catch 
+        _:_ -> undefined
+    end;
+
+%% Other cases: context, dict or parametrized module lookup.
+find_value(Key, Tuple) when is_tuple(Tuple) ->
+    Module = element(1, Tuple),
+    case Module of
+        dict -> 
+            case dict:find(Key, Tuple) of
+                {ok, Val} ->
+                    Val;
+                _ ->
+                    undefined
+            end;
+        Module ->
+            Exports = Module:module_info(exports),
+            case proplists:get_value(Key, Exports) of
+                1 ->
+                    Tuple:Key();
+                _ ->
+                    case proplists:get_value(get, Exports) of
+                        1 -> 
+                            Tuple:get(Key);
+                        _ ->
+                            undefined
+                    end
+            end
+    end;
+
+%% Any subvalue of a non-existant value is empty
+find_value(_Key, _Data) ->
+	undefined.
+
+
 %% @doc Cleanup process for the depcache.  Periodically checks a batch of depcache items for their validity.
 %%      Asks the depcache server to delete invalidated items.  When the load of the data table is too high then
 %%      This cleanup process starts to delete random entries.  By using a random delete we don't need to keep
 %%      a LRU list, which is a bit expensive.
 
-cleanup() ->
+cleanup(Pid) ->
     {A1,A2,A3} = now(),
     random:seed(A1, A2, A3),
-    ?MODULE:cleanup(0, zp_utils:now(), normal, 0).
+    ?MODULE:cleanup(Pid, 0, zp_utils:now(), normal, 0).
 
 %% Wrap around the end of table
-cleanup('$end_of_table', Now, _Mode, Ct) ->
+cleanup(Pid, '$end_of_table', Now, _Mode, Ct) ->
     case ets:info(?META_TABLE, size) of
-        0 -> ?MODULE:cleanup(0, Now, cleanup_mode(), 0);
-        _ -> ?MODULE:cleanup(0, Now, cleanup_mode(), Ct)
+        0 -> ?MODULE:cleanup(Pid, 0, Now, cleanup_mode(Pid), 0);
+        _ -> ?MODULE:cleanup(Pid, 0, Now, cleanup_mode(Pid), Ct)
     end;
 
 %% In normal cleanup, sleep a second between each batch before continuing our cleanup sweep
-cleanup(SlotNr, Now, normal, 0) -> 
+cleanup(Pid, SlotNr, Now, normal, 0) -> 
     timer:sleep(1000),
     case ets:info(?META_TABLE, size) of
-        0 -> ?MODULE:cleanup(0, Now, normal, 0);
-        _ -> ?MODULE:cleanup(SlotNr, zp_utils:now(), cleanup_mode(), ?CLEANUP_BATCH)
+        0 -> ?MODULE:cleanup(Pid, 0, Now, normal, 0);
+        _ -> ?MODULE:cleanup(Pid, SlotNr, zp_utils:now(), cleanup_mode(Pid), ?CLEANUP_BATCH)
     end;
 
 %% After finishing a batch in cache_full mode, check if the cache is still full, if so keep deleting entries
-cleanup(SlotNr, Now, cache_full, 0) -> 
-    case cleanup_mode() of
-        normal     -> ?MODULE:cleanup(SlotNr, Now, normal, 0);
-        cache_full -> ?MODULE:cleanup(SlotNr, zp_utils:now(), cache_full, ?CLEANUP_BATCH)
+cleanup(Pid, SlotNr, Now, cache_full, 0) -> 
+    case cleanup_mode(Pid) of
+        normal     -> ?MODULE:cleanup(Pid, SlotNr, Now, normal, 0);
+        cache_full -> ?MODULE:cleanup(Pid, SlotNr, zp_utils:now(), cache_full, ?CLEANUP_BATCH)
     end;
 
 %% Normal cleanup behaviour - check expire stamp and dependencies
-cleanup(SlotNr, Now, normal, Ct) ->
+cleanup(Pid, SlotNr, Now, normal, Ct) ->
     Slot =  try 
                 ets:slot(?META_TABLE, SlotNr)
             catch
                 _M:_E -> '$end_of_table'
             end,
     case Slot of
-        '$end_of_table' -> ?MODULE:cleanup('$end_of_table', Now, normal, 0);
-        [] -> ?MODULE:cleanup(SlotNr+1, Now, normal, Ct-1);
+        '$end_of_table' -> ?MODULE:cleanup(Pid, '$end_of_table', Now, normal, 0);
+        [] -> ?MODULE:cleanup(Pid, SlotNr+1, Now, normal, Ct-1);
         Entries ->
             lists:foreach(fun(Meta) -> flush_expired(Meta, Now) end, Entries),
-            ?MODULE:cleanup(SlotNr+1, Now, normal, Ct-1)
+            ?MODULE:cleanup(Pid, SlotNr+1, Now, normal, Ct-1)
     end;
 
 %% Full cache cleanup mode - randomly delete every 10th entry
-cleanup(SlotNr, Now, cache_full, Ct) ->
+cleanup(Pid, SlotNr, Now, cache_full, Ct) ->
     Slot =  try 
                 ets:slot(?META_TABLE, SlotNr)
             catch
                 _M:_E -> '$end_of_table'
             end,
     case Slot of
-        '$end_of_table' -> ?MODULE:cleanup('$end_of_table', Now, cache_full, 0);
-        [] -> ?MODULE:cleanup(SlotNr+1, Now, cache_full, Ct-1);
+        '$end_of_table' -> ?MODULE:cleanup(Pid, '$end_of_table', Now, cache_full, 0);
+        [] -> ?MODULE:cleanup(Pid, SlotNr+1, Now, cache_full, Ct-1);
         Entries ->
             FlushExpire = fun (Meta) ->
                                 case flush_expired(Meta, Now) of
@@ -264,7 +377,7 @@ cleanup(SlotNr, Now, cache_full, Ct) ->
 
             Entries1 = lists:map(FlushExpire, Entries),
             lists:foreach(RandomDelete, Entries1),
-            ?MODULE:cleanup(SlotNr+1, Now, cache_full, Ct-1)
+            ?MODULE:cleanup(Pid, SlotNr+1, Now, cache_full, Ct-1)
     end.
 
 
@@ -277,10 +390,13 @@ flush_expired(#meta{key=Key, serial=Serial, expire=Expire, depend=Depend}, Now) 
     end.
 
 
-%% @doc When the data table is too large then this function randomly deletes keys.  It also signals the cleanup process
-%%      That it needs to be more aggressive, upping its batch size.
-cleanup_mode() ->
-    Memory = ets:info(?DATA_TABLE, memory),
+%% @doc When the data table is too large then we start to randomly delete keys.  It also signals the cleanup process
+%% that it needs to be more aggressive, upping its batch size.
+%% We use erts_debug:size() on the stored terms to calculate the total size of all terms stored.  This
+%% is better than counting the number of entries.  Using the process_info(Pid,memory) is not very useful as the
+%% garbage collection still needs to be done and then we delete too many entries.
+cleanup_mode(_Pid) ->
+    Memory = ?MODULE:size(),
     if 
         Memory >= ?MEMORY_MAX -> cache_full;
         true -> normal
