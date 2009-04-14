9545c39ea75bedf1686da9260c2cdfe80fa7f708
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-04-14 20:19:45 +0000

    Allow use of the dispatcher by using:
    action={redirect  dispatch="rulename" arg=...}
    
    
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@214 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- src/actions/action_redirect.erl
+++ src/actions/action_redirect.erl
@@ -3,15 +3,25 @@
 %%
 %% @doc Perform a redirect to another page.
 %% @todo Make this a better redirect by including the host name of the site
+%% @todo Allow the location/id types of redirect to have extra arguments
 
 -module(action_redirect).
 -include("zophrenic.hrl").
 -export([render_action/4]).
 
-render_action(_TriggerId, _TargetId, Args, Context) -> 
-    Location = case proplists:get_value(id, Args) of
-        undefined -> proplists:get_value(location, Args, "/");
-        Id -> m_rsc:p(Id, page_url, Context)
+render_action(_TriggerId, _TargetId, Args, Context) ->
+    Location = case proplists:get_value(dispatch, Args) of
+        undefined ->
+            case proplists:get_value(id, Args) of
+                undefined -> 
+                    proplists:get_value(location, Args, "/");
+                Id ->
+                    m_rsc:p(Id, page_url, Context)
+            end;
+        DispatchString ->
+            Dispatch = zp_convert:to_atom(DispatchString),
+            Args1 = proplists:delete(dispatch, Args),
+            zp_dispatcher:url_for(Dispatch, Args1, Context)
     end,
 	Script   = [<<"window.location = \"">>,zp_utils:js_escape(Location),$",$;],
 	{Script, Context}.
