7f37ffe36a07f0227ec4bd5e29c2301f9c55137b
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-04-27 20:07:18 +0000

    Added extra escape functions, also escapes now property lists.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@417 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- src/support/zp_html.erl
+++ src/support/zp_html.erl
@@ -9,12 +9,85 @@
 
 %% interface functions
 -export([
+    escape_props/1,
+    escape/1,
     strip/1
 ]).
 
 -include_lib("zophrenic.hrl").
 
 
+%% @doc Escape all properties used for an update statement. Only leaves the body property in tact.
+%% @spec escape_props(PropertyList) -> PropertyList
+escape_props(Props) ->
+    escape_props(Props, []).
+
+    escape_props([], Acc) ->
+        Acc;
+    escape_props([{_K,V} = Prop|T], Acc) when is_float(V); is_integer(V); is_atom(V) -> 
+        escape_props(T, [Prop|Acc]);
+    escape_props([{body, V}|T], Acc) ->
+        escape_props(T, [{body, V} | Acc]);
+    escape_props([{K, V}|T], Acc) ->
+        escape_props(T, [{K, escape_value(V)} | Acc]).
+
+    escape_value({trans, Texts}) ->
+        {trans, escape_props(Texts)};
+    escape_value(V) when is_list(V) ->
+        try
+            escape_value(iolist_to_binary(V))
+        catch _:_ ->
+            V
+        end;
+    escape_value(B) when is_binary(B) ->
+        escape(B);
+    escape_value(V) -> 
+        V.
+
+
+%% @doc Special html escape function that leaves some entities in tact, making it save to escape a text multiple times.
+%% @spec escape(iolist()) -> iolist()
+escape(undefined) -> 
+    undefined;
+escape(<<>>) -> 
+    <<>>;
+escape([]) ->
+    [];
+escape(L) when is_list(L) ->
+    escape(list_to_binary(L));
+escape(B) when is_binary(B) ->
+    escape(B, <<>>).
+
+    escape(<<>>, Acc) -> 
+        Acc;
+    escape(<<"&amp;", T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "&amp;">>);
+    escape(<<"&quot;", T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "&quot;">>);
+    escape(<<"&#39;", T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "&#39;">>);
+    escape(<<"&lt;", T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "&lt;">>);
+    escape(<<"&gt;", T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "&gt;">>);
+    escape(<<"&euro;", T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "€">>);
+    escape(<<$&, T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "&amp;">>);
+    escape(<<$<, T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "&lt;">>);
+    escape(<<$>, T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "&gt;">>);
+    escape(<<$", T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "&quot;">>);
+    escape(<<$', T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, "&#39;">>);
+    escape(<<C, T/binary>>, Acc) ->
+        escape(T, <<Acc/binary, C>>).
+
+
+%% @doc Strip all html elements from the text. Simple parsing is applied to find the elements. Does not escape the end result.
+%% @spec strip(iolist()) -> iolist()
 strip(<<>>) ->
     <<>>;
 strip([]) ->
