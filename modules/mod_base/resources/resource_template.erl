203c46e1509b2d1c5e08717a7b444e1f0d39c83e
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-06-11 16:00:53 +0000

    Added simple /helloworld uri in the test module for benchmarking.  Fixed template resource..
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@555 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_base/resources/resource_template.erl
+++ modules/mod_base/resources/resource_template.erl
@@ -5,7 +5,7 @@
 -module(resource_template).
 -author("Marc Worrell <marc@worrell.nl>").
 
--export([init/1, to_html/1, service_available/2, charsets_provided/2]).
+-export([init/1, to_html/2, service_available/2, charsets_provided/2]).
 
 -include_lib("webmachine_resource.hrl").
 -include_lib("include/zophrenic.hrl").
@@ -21,8 +21,10 @@ service_available(ReqData, Settings) ->
 charsets_provided(ReqData, Context) ->
     {[{"utf-8", fun(X) -> X end}], ReqData, Context}.
 
-to_html(Context) ->
-    Context1 = zp_context:ensure_all(Context),
-    Template = zp_context:get(template, Context1),
-    Html     = zp_template:render(Template, [], Context1),
-    zp_context:output(Html, Context1).
+to_html(ReqData, Context) ->
+    Context1 = ?WM_REQ(ReqData, Context),
+    Context2 = zp_context:ensure_all(Context1),
+    Template = zp_context:get(template, Context2),
+    Rendered = zp_template:render(Template, [], Context2),
+    {Html, Context3} = zp_context:output(Rendered, Context2),
+    ?WM_REPLY(Html, Context3).
