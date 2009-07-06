da8d2dee5eab8da589f70acde898908e654bd720
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-07-06 22:25:24 +0000

    Added possibility to pass selected id to an action. Also added possibility to choose another template for the result list rendering. Added result template that submit the selected entry.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@608 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_base/actions/action_base_typeselect.erl
+++ modules/mod_base/actions/action_base_typeselect.erl
@@ -15,10 +15,10 @@
 ]).
 
 render_action(TriggerId, TargetId, Args, Context) ->
-    SubjectId = zp_convert:to_integer(proplists:get_value(subject_id, Args)),
-    Predicate = proplists:get_value(predicate, Args),
     Actions = proplists:get_all_values(action, Args),
-    Postback = {typeselect, SubjectId, Predicate, Actions},
+    ActionsWithId = proplists:get_all_values(action_with_id, Args),
+    Template = proplists:get_value(template, Args, "_action_typeselect_result.tpl"),
+    Postback = {typeselect, Template, Actions, ActionsWithId},
 	{_PostbackMsgJS, PickledPostback} = zp_render:make_postback(Postback, key, TriggerId, TargetId, ?MODULE, Context),
 	JS = [
 	    <<"zp_typeselect(\"">>, TriggerId, $",$,,$", PickledPostback, <<"\");">>
@@ -28,14 +28,13 @@ render_action(TriggerId, TargetId, Args, Context) ->
 
 %% @doc Unlink the edge, on success show an undo message in the element with id "unlink-message"
 %% @spec event(Event, Context1) -> Context2
-event({postback, {typeselect, SubjectId, Predicate, Actions}, _TriggerId, TargetId}, Context) ->
+event({postback, {typeselect, Template, Actions, ActionsWithId}, _TriggerId, TargetId}, Context) ->
     Text = zp_context:get_q("triggervalue", Context),
     SearchResult = zp_search:search({autocomplete, [{text, Text}]}, {1,20}, Context),
     Vars = [
-        {subject_id, SubjectId},
-        {predicate, Predicate},
         {result, SearchResult#search_result.result},
-        {action, Actions}
+        {action, Actions},
+        {action_with_id, ActionsWithId}
     ],
-    Html = zp_template:render("_action_typeselect_result.tpl", Vars, Context),
+    Html = zp_template:render(Template, Vars, Context),
     zp_render:update(TargetId, Html, Context).
