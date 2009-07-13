d407f0c3d9d56252d80b23ecc3a1f8bb7681e4db
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-07-13 10:31:19 +0000

    Added a dialog to add a category.
    Moved the inline style sheet to the main category sorter page.
    
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@671 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

---
+++ modules/mod_admin_category/actions/action_admin_category_dialog_category_add.erl
@@ -0,0 +1,67 @@
+%% @author Marc Worrell <marc@worrell.nl>
+%% @copyright 2009 Marc Worrell
+%% @date 2009-07-13
+%%
+%% @doc Open a dialog to let the user add a new category. Allows to select the parent category, refreshes the category overview.
+
+-module(action_admin_category_dialog_category_add).
+-author("Marc Worrell <marc@worrell.nl").
+
+%% interface functions
+-export([
+    render_action/4,
+    event/2
+]).
+
+-include("zophrenic.hrl").
+
+render_action(TriggerId, TargetId, Args, Context) ->
+    OnSuccess = proplists:get_all_values(on_success, Args),
+    Postback = {dialog_category_add, OnSuccess},
+	{PostbackMsgJS, _PickledPostback} = zp_render:make_postback(Postback, click, TriggerId, TargetId, ?MODULE, Context),
+	{PostbackMsgJS, Context}.
+
+
+%% @doc Fill the dialog with the delete confirmation template. The next step will ask to delete the category.
+%% @spec event(Event, Context1) -> Context2
+event({postback, {dialog_category_add, OnSuccess}, _TriggerId, _TargetId}, Context) ->
+    case zp_acl:has_role(admin, Context) of
+        true ->
+            Vars = [ {on_success, OnSuccess} ],
+            zp_render:dialog("Add category", "_action_dialog_category_add.tpl", Vars, Context);
+        false ->
+            zp_render:growl_error("Only administrators can add categories.", Context)
+    end;
+
+%% @doc Handle the form postback. Optionally renaming existing categories.
+event({submit, {category_add, Options}, _TriggerId, _TargetId}, Context) ->
+    case zp_acl:has_role(admin, Context) of
+        true ->
+            Title    = zp_context:get_q_validated("title", Context),
+            Name     = zp_context:get_q_validated("name", Context),
+            GroupId  = zp_convert:to_integer(zp_context:get_q("group_id", Context, undefined)),
+            ParentId = zp_convert:to_integer(zp_context:get_q("category_id", Context, undefined)),
+            
+            Props = [
+                {group_id, GroupId},
+                {category, category},
+                {name, Name},
+                {title, Title}
+            ],
+            
+            case m_rsc:insert(Props, Context) of
+                {ok, Id} ->
+                    case ParentId of
+                        PId when is_integer(PId) ->  m_category:move_below(Id, PId, Context);
+                        undefined -> nop
+                    end,
+                    zp_render:wire(proplists:get_all_values(on_success, Options), Context);
+                {error, duplicate_name} ->
+                    zp_render:growl_error("This category exists already. Please use another name.", Context);
+                {error, Reason} ->
+                    Error = io_lib:format("Could not insert the categorie (~p)", [Reason]),
+                    zp_render:growl_error(Error, Context)
+            end;
+        false ->
+            zp_render:growl_error("Only administrators can delete categories.", Context)
+    end.
--- modules/mod_admin_category/templates/admin_category_sorter.tpl
+++ modules/mod_admin_category/templates/admin_category_sorter.tpl
@@ -15,6 +15,42 @@ Category Hierarchy
 			The categories are defined in a hierarchy.  Here you can change that hierarchy.
 		</p>
 
+		<p>
+			{% button text="New Category" action={dialog_category_add on_success={reload}} %}
+		</p>
+		
+		<div class="clearfix">&nbsp;</div>
+
+		<style>
+			.hover a, .hover {
+				background: #aac;
+				color: black !important;
+			}
+			
+			.short-list .buttons {
+ 				float: right;
+			}
+	
+			.ui-draggable-dragging .buttons {
+				display: none;
+			}
+
+			.short-list .line {
+				height: 5px;
+			}
+			
+			.short-list li.depth-1 { margin-left: 0px; }
+			.short-list li.depth-2 { margin-left: 30px; }
+			.short-list li.depth-3 { margin-left: 60px; }
+			.short-list li.depth-4 { margin-left: 90px; }
+			.short-list li.depth-5 { margin-left: 120px; }
+			.short-list li.depth-7 { margin-left: 150px; }
+			.short-list li.depth-8 { margin-left: 180px; }
+			.short-list li.depth-9 { margin-left: 210px; }
+			.short-list li.depth-10 { margin-left: 240px; }
+			.short-list li.depth-11 { margin-left: 270px; }
+		</style>
+		
 		<div id="category-sorter">
 			{% include "_admin_category_sorter.tpl" %}
 		</div>
