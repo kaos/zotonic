f0cd8be995d631a822cd260fdb578128c86e86d3
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-07-19 11:18:51 +0000

    Pass all dispatch vars to template.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@750 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_base/resources/resource_template.erl
+++ modules/mod_base/resources/resource_template.erl
@@ -5,26 +5,12 @@
 -module(resource_template).
 -author("Marc Worrell <marc@worrell.nl>").
 
--export([init/1, to_html/2, service_available/2, charsets_provided/2]).
+-export([to_html/1]).
 
--include_lib("webmachine_resource.hrl").
--include_lib("include/zophrenic.hrl").
+-include_lib("resource_html.hrl").
+-include_lib("zophrenic.hrl").
 
-init(Settings) -> {ok, Settings}.
-
-service_available(ReqData, Settings) ->
-    {template, Template} = proplists:lookup(template, Settings),
-    Context  = zp_context:new(ReqData, ?MODULE),
-    Context1 = zp_context:set(template, Template, Context),
-    ?WM_REPLY(true, Context1).
-
-charsets_provided(ReqData, Context) ->
-    {[{"utf-8", fun(X) -> X end}], ReqData, Context}.
-
-to_html(ReqData, Context) ->
-    Context1 = ?WM_REQ(ReqData, Context),
-    Context2 = zp_context:ensure_all(Context1),
-    Template = zp_context:get(template, Context2),
-    Rendered = zp_template:render(Template, [], Context2),
-    {Html, Context3} = zp_context:output(Rendered, Context2),
-    ?WM_REPLY(Html, Context3).
+to_html(Context) ->
+    {template, Template} = zp_context:lookup(template, Context),
+    Rendered = zp_template:render(Template, zp_context:get_all(Context), Context),
+    zp_context:output(Rendered, Context).
