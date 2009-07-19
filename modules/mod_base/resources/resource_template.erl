510e4a4cf5f859155c1f49227398cfb472b28feb
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-07-19 11:19:22 +0000

    Removed includes.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@751 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_base/resources/resource_template.erl
+++ modules/mod_base/resources/resource_template.erl
@@ -7,9 +7,6 @@
 
 -export([to_html/1]).
 
--include_lib("resource_html.hrl").
--include_lib("zophrenic.hrl").
-
 to_html(Context) ->
     {template, Template} = zp_context:lookup(template, Context),
     Rendered = zp_template:render(Template, zp_context:get_all(Context), Context),
