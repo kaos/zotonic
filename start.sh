efe367ad5bf9bfe5f094cb478756333bc5216ce8
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-07-20 13:58:43 +0000

    Renaming the files to z_ instead of zp_
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@785 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export ZOPHRENIC_SIGN_KEY_SIMPLE=gipfelstuermer
+export ZOTONIC_SIGN_KEY_SIMPLE=gipfelstuermer
 cd `dirname $0`
-exec erl +P 10000000 +K true -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s zophrenic
+exec erl +P 10000000 +K true -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s zotonic
