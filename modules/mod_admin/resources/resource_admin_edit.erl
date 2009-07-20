70525ed97b27b88fd3cdb3fe09b23b343da79d36
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-07-20 06:37:50 +0000

    Reload the page when the category is changed, this displays the category dependent input fields.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@773 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_admin/resources/resource_admin_edit.erl
+++ modules/mod_admin/resources/resource_admin_edit.erl
@@ -46,21 +46,27 @@ event({submit, rscform, _FormId, _TargetId}, Context) ->
     Title = ?TR(proplists:get_value("title", Props), Context),
     Id = zp_convert:to_integer(proplists:get_value("id", Props)),
     Props1 = proplists:delete("id", Props),
+    CatBefore = m_rsc:p(Id, category_id, Context),
     case m_rsc:update(Id, Props1, Context) of
         {ok, _} -> 
-            Context1 = zp_render:set_value("field-name", m_rsc:p(Id, name, Context), Context),
-            Context2 = zp_render:set_value("field-uri",  m_rsc:p(Id, uri, Context1), Context1),
-            Context3 = zp_render:set_value("slug",  m_rsc:p(Id, slug, Context2), Context2),
-            Context4 = case zp_convert:to_bool(m_rsc:p(Id, is_protected, Context3)) of
-                true ->  zp_render:wire("delete-button", {disable, []}, Context3);
-                false -> zp_render:wire("delete-button", {enable, []}, Context3)
-            end,
             case proplists:is_defined("save_view", Post) of
                 true ->
                     PageUrl = m_rsc:p(Id, page_url, Context),
                     zp_render:wire({redirect, [{location, PageUrl}]}, Context);
                 false ->
-                    zp_render:growl(["Saved ",zp_html:strip(Title),"."], Context4)
+                    case m_rsc:p(Id, category_id, Context) of
+                        CatBefore ->
+                            Context1 = zp_render:set_value("field-name", m_rsc:p(Id, name, Context), Context),
+                            Context2 = zp_render:set_value("field-uri",  m_rsc:p(Id, uri, Context1), Context1),
+                            Context3 = zp_render:set_value("slug",  m_rsc:p(Id, slug, Context2), Context2),
+                            Context4 = case zp_convert:to_bool(m_rsc:p(Id, is_protected, Context3)) of
+                                true ->  zp_render:wire("delete-button", {disable, []}, Context3);
+                                false -> zp_render:wire("delete-button", {enable, []}, Context3)
+                            end,
+                            zp_render:growl(["Saved ",zp_html:strip(Title),"."], Context4);
+                        _CatOther ->
+                            zp_render:wire({reload, []}, Context)
+                    end
             end;
         {error, duplicate_uri} ->
             zp_render:growl_error("Error, duplicate uri. Please change the uri.", Context);
