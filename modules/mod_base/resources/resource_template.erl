00d347ab4508c0b183bf80a20788435c9783c79b
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-08-14 09:38:31 +0000

    Added 'content_type' option to resource_template. This enables generation of xml files.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@1029 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_base/resources/resource_template.erl
+++ modules/mod_base/resources/resource_template.erl
@@ -5,11 +5,35 @@
 -module(resource_template).
 -author("Marc Worrell <marc@worrell.nl>").
 
--export([html/1]).
+-export([init/1, service_available/2, charsets_provided/2, content_types_provided/2]).
+-export([provide_content/2]).
 
--include_lib("resource_html.hrl").
+-include_lib("webmachine_resource.hrl").
+-include_lib("include/zotonic.hrl").
 
-html(Context) ->
-    Template = z_context:get(template, Context),
-    Rendered = z_template:render(Template, z_context:get_all(Context), Context),
-    z_context:output(Rendered, Context).
+
+init(DispatchArgs) -> {ok, DispatchArgs}.
+
+service_available(ReqData, DispatchArgs) when is_list(DispatchArgs) ->
+    Context  = z_context:new(ReqData, ?MODULE),
+    Context1 = z_context:set(DispatchArgs, Context),
+    ?WM_REPLY(true, Context1).
+
+charsets_provided(ReqData, Context) ->
+    {[{"utf-8", fun(X) -> X end}], ReqData, Context}.
+
+content_types_provided(ReqData, Context) ->
+    case z_context:get(content_type, Context) of
+        undefined ->
+            {[{"text/html", provide_content}], ReqData, Context};
+        Mime -> 
+            {[{Mime, provide_content}], ReqData, Context}
+    end.
+
+provide_content(ReqData, Context) ->
+    Context1 = ?WM_REQ(ReqData, Context),
+    Context2 = z_context:ensure_all(Context1),
+    Template = z_context:get(template, Context2),
+    Rendered = z_template:render(Template, z_context:get_all(Context), Context2),
+    {Output, OutputContext} = z_context:output(Rendered, Context2),
+    ?WM_REPLY(Output, OutputContext).
