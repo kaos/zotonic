378d7c84e29ea7e927b0cf2853c280980214a247
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-08-19 07:08:13 +0000

    Small changes to make benchmarking on '/helloworld' a bit more representative.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@1054 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_test/mod_test.erl
+++ modules/mod_test/mod_test.erl
@@ -8,6 +8,11 @@
 -author("Marc Worrell <marc@worrell.nl>").
 -behaviour(gen_server).
 
+-mod_title("Test").
+-mod_description("Simple tests - no need to enable this module.").
+-mod_prio(1).
+
+
 %% gen_server exports
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 -export([start_link/0, start_link/1]).
