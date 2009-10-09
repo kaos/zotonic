a0ac97bc1b980135b66d1f3924cff7fdebb38d06
arjan <arjan@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-10-09 08:04:14 +0000

    Webmachine error handler: simplified previous checkin.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@1140 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- src/support/z_webmachine_error_handler.erl
+++ src/support/z_webmachine_error_handler.erl
@@ -14,31 +14,28 @@
 
 render_error(Code=404, Req, _Reason) ->
     ErrorDump = mochiweb_html:escape(lists:flatten(io_lib:format("Resource not found: ~p", [Req:raw_path()]))),
-    Handler = error_map(list_to_atom(Req:get_metadata('content-type'))),
-    Handler(Req, Code, ErrorDump);
+    Type = list_to_atom(Req:get_metadata('content-type')),
+    error_handler(Type, Req, Code, ErrorDump);
 
 render_error(Code=500, Req, Reason) ->
     error_logger:error_msg("webmachine error: path=~p~n~p~n", [Req:path(), Reason]),
     ErrorDump = mochiweb_html:escape(lists:flatten(io_lib:format("~p", [Reason]))),
-    Handler = error_map(Req:get_metadata('content-type')),
-    Handler(Req, Code, ErrorDump).
+    Type = list_to_atom(Req:get_metadata('content-type')),
+    error_handler(Type, Req, Code, ErrorDump).
 
 
-error_map('application/json') ->
-    fun(Req, Code, ErrorDump) ->
-            Req:add_response_header("Content-Type", "application/json; charset=utf-8"),
-            Req:add_response_header("Content-Encoding", "identity"),
-            JS = {struct, [{error_code, Code}, {error_dump, ErrorDump}]},
-            mochijson:encode(JS)
-    end;
+error_handler('application/json', Req, Code, ErrorDump) ->
+    Req:add_response_header("Content-Type", "application/json; charset=utf-8"),
+    Req:add_response_header("Content-Encoding", "identity"),
+    JS = {struct, [{error_code, Code}, {error_dump, ErrorDump}]},
+    mochijson:encode(JS);
 
-error_map(_) ->
-    fun (Req, Code, ErrorDump) ->
-            Req:add_response_header("Content-Type", "text/html; charset=utf-8"),
-            Req:add_response_header("Content-Encoding", "identity"),
-            Context = z_context:new(Req:get_metadata('host')),
-            Vars = [{error_code, Code}, {error_dump, ErrorDump}],
-            Html = z_template:render("error.tpl", Vars, Context),
-            {Output, _} = z_context:output(Html, Context),
-            Output
-    end.
+error_handler(_Default, Req, Code, ErrorDump) ->
+    Req:add_response_header("Content-Type", "text/html; charset=utf-8"),
+    Req:add_response_header("Content-Encoding", "identity"),
+    Context = z_context:new(Req:get_metadata('host')),
+    Vars = [{error_code, Code}, {error_dump, ErrorDump}],
+    Html = z_template:render("error.tpl", Vars, Context),
+    {Output, _} = z_context:output(Html, Context),
+    Output.
+
