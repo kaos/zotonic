fcb36636487dcd0690887fc7356b4f95bcd6d9d1
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-10-11 08:24:15 +0000

    search (all_bytitle) gets a list for the cat name and the list has length 1, then just assume that the element in the list is a category.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@1155 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_search/support/search_all_bytitle.erl
+++ modules/mod_search/support/search_all_bytitle.erl
@@ -16,7 +16,7 @@
 
 -include_lib("zotonic.hrl").
 
-search([Cat], Context) when is_atom(Cat) ->
+search([Cat], Context) ->
     search(Cat, Context);
 search(Cat, Context) ->
     case m_category:name_to_id(Cat, Context) of
