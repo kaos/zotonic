8d00126433a36e4be0a0e2e1a8793852f27dbcba
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-10-19 09:53:39 +0000

    Fix for img tag of spinner.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@1211 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_base/scomps/scomp_base_spinner.erl
+++ modules/mod_base/scomps/scomp_base_spinner.erl
@@ -16,4 +16,4 @@ terminate(_Reason) -> ok.
 
 render(Params, _Vars, _Context, _State) ->
     Image = proplists:get_value(image, Params, <<"/lib/images/spinner.gif">>),
-    {ok, <<"<div id=\"spinner\" class=\"spinner\" style=\"display: none\"><image alt=\"activity indicator\" src=\"">>,Image,<<"\" /></div>">>}.
+    {ok, <<"<div id=\"spinner\" class=\"spinner\" style=\"display: none\"><img alt=\"activity indicator\" src=\"">>,Image,<<"\" /></div>">>}.
