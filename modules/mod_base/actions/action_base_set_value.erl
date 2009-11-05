238b398949d4a1d5ffc442d8a1151a83c40ac7e3
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-11-05 19:10:33 +0000

    Escape the value string.
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@1314 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_base/actions/action_base_set_value.erl
+++ modules/mod_base/actions/action_base_set_value.erl
@@ -11,4 +11,4 @@ render_action(_TriggerId, TargetId, Args, Context) ->
     Id = proplists:get_value(id, Args, TargetId),
     Value = proplists:get_value(value, Args, ""),
     ValueString = z_convert:to_list(Value),
-    {[ $$, $(, $", $#, z_utils:js_escape(Id), <<"\").val(\"">>, ValueString, $", $), $;  ], Context}.
+    {[ $$, $(, $", $#, z_utils:js_escape(Id), <<"\").val(\"">>, z_utils:js_escape(ValueString), $", $), $;  ], Context}.
