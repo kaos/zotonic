e5bb7bd8a678f601c2ab94ea2c0987d6c303c4f3
marc <marc@39ac7d6a-fe55-4cd0-aea4-b13bf11f1711>
2009-11-09 16:07:08 +0000

    Modules can now add content types.
    A foldr of the 'content_types_dispatch' notification is used to collect all provided content types. They should return a 2-tuple like {"text/plain", my_dispatch_rule_name}
    
    git-svn-id: https://Neo:8443/svn/zotonic/trunk@1343 39ac7d6a-fe55-4cd0-aea4-b13bf11f1711

--- modules/mod_base/resources/resource_id.erl
+++ modules/mod_base/resources/resource_id.erl
@@ -12,29 +12,58 @@
 
 -export([
     init/1,
+	service_available/2,
     resource_exists/2,
     content_types_provided/2,
-    to_html/2
+    see_other/2
 ]).
 
 -include_lib("webmachine_resource.hrl").
 -include_lib("zotonic.hrl").
 
-init([]) -> {ok, []}.
+init(DispatchArgs) -> {ok, DispatchArgs}.
 
-resource_exists(ReqData, _Context) ->
-    Context = z_context:new(ReqData, ?MODULE),
-    ContextQs = z_context:ensure_qs(Context),
+service_available(ReqData, DispatchArgs) when is_list(DispatchArgs) ->
+    Context  = z_context:new(ReqData, ?MODULE),
+    Context1 = z_context:set(DispatchArgs, Context),
+    ?WM_REPLY(true, Context1).
+
+resource_exists(ReqData, Context) ->
+    Context1 = ?WM_REQ(ReqData, Context),
+    ContextQs = z_context:ensure_qs(Context1),
     Id = z_context:get_q("id", ContextQs),
     ?WM_REPLY(m_rsc:exists(Id, ContextQs), ContextQs).
 
 content_types_provided(ReqData, Context) ->
-   {[{"text/html", to_html}], ReqData, Context}.
- 
-to_html(ReqData, Context) ->
-    Id       = z_context:get_q("id", Context),
-    Location = m_rsc:p_no_acl(Id, page_url, Context),
-    Url = "http://" ++ wrq:get_req_header("host", ReqData) ++ Location,
-    ReqData1 = wrq:set_resp_header("Location", Url, ReqData),
-    ReqData2 = wrq:set_response_code(303, ReqData1),
-    {{halt, 303}, ReqData2, Context}.
+	{CT,Context1} = get_content_types(Context),
+	CT1 = [{Mime, see_other} || {Mime, _Dispatch} <- CT],
+	{CT1, ReqData, Context1}.
+
+see_other(ReqData, Context) ->
+	Mime = wrq:get_resp_header("Content-Type", ReqData),
+    Context1 = ?WM_REQ(ReqData, Context),
+	{CT,Context2} = get_content_types(Context1),
+    Id = z_context:get_q("id", Context2),
+	Location = case proplists:get_value(Mime, CT) of
+					page_url -> m_rsc:p_no_acl(Id, page_url, Context2);
+					Dispatch -> z_dispatcher:url(Dispatch, [{id,Id}], Context2)
+			   end,
+	AbsUrl = z_context:abs_url(Location, Context2),
+    Context3 = z_context:set_resp_header("Location", AbsUrl, Context2),
+	?WM_REPLY({halt, 303}, Context3).
+
+%% @doc Fetch the list of content types provided, together with their dispatch rule name.
+get_content_types(Context) ->
+	case z_context:get(content_types_dispatch, Context) of
+		undefined ->
+			CT = z_notifier:foldr({content_types_dispatch}, [], Context),
+			?DEBUG(CT),
+			CT1 = case proplists:get_value("text/html", CT) of
+					undefined -> [{"text/html", page_url}|CT];
+					Prov -> [Prov|CT]
+				  end,
+			Context1 = z_context:set(content_types_dispatch, CT1, Context),
+			{CT1, Context1};
+		CT -> 
+			{CT, Context}
+	end.
